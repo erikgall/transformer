6d81b406fb07b3cb8a51aa61cbddb136c269f1ad
Erik Galloway <emg5478@psu.edu>
2016-07-05 23:21:58 -0500

    Allow the tranformable model trait to find the transformers with setting the property

--- src/Traits/TransformableModel.php
+++ src/Traits/TransformableModel.php
@@ -51,6 +51,11 @@ trait TransformableModel
     protected function transformerClass()
     {
 
+        if (class_exists($default = $this->getDefaultTransformerNamespace())){
+
+            return $default;
+        }
+
         if (property_exists($this, 'transformer')) {
 
             return $this->transformer;
@@ -58,5 +63,36 @@ trait TransformableModel
         }
 
         return Transformer::class;
+    }
+
+    /**
+     * Get the default transformer class for the model.
+     *
+     * @return string
+     */
+    protected function getDefaultTransformerNamespace()
+    {
+
+        $parts = explode('\\', get_class($this));
+
+        $class = array_last($parts);
+
+        array_forget($parts, array_last(array_keys($parts)));
+
+        return $this->makeTransformer($class, implode('\\', $parts));
+
+    }
+
+    /**
+     * Make the default transformer class namespace.
+     *
+     * @param string $class
+     * @param string $namespace
+     * @return string
+     */
+    protected function makeTransformer($class, $namespace)
+    {
+
+        return $namespace . '\\Transformers\\' . $class . 'Transformer';
     }
 }
